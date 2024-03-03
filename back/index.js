ceb634fd7d9fd89550a52a1a8c8621c453614986
fsilber <fsilber@gmail.com>
2024-03-03 13:19:54 -0300

    imp mongoose

--- back/index.js
+++ back/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import {Schema, model} from 'mongoose'
+import {Schema, model, mongoose} from 'mongoose'
 
 const app = express();
 mongoose.connect('mongodb+srv://fransilber16:KMKtQn2qMyR1OU7w@cluster0.8nbblqs.mongodb.net/pruebadeploy?retryWrites=true&w=majority&appName=Cluster0');
