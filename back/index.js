bec327e8ada6971133bf1c8c8811009d4386be77
fsilber <fsilber@gmail.com>
2024-03-03 13:17:35 -0300

    implementado mongo

--- back/index.js
+++ back/index.js
@@ -1,18 +1,28 @@
 import express from 'express'
 import cors from 'cors'
+import {Schema, model} from 'mongoose'
 
 const app = express();
-
+mongoose.connect('mongodb+srv://fransilber16:KMKtQn2qMyR1OU7w@cluster0.8nbblqs.mongodb.net/pruebadeploy?retryWrites=true&w=majority&appName=Cluster0');
 app.use(cors())
+
+const objectSchema = new Schema({
+    name: String
+})
+
+const Object = model('obj', objectSchema)
+
+const newObject = {
+    name: "hola3"
+}
+
+Object.save(newObject)
 
 app.get('/api', (req, res) => {
 
-    const objs = [
-        {name: "holaa"},
-        {name: "hola2"}
-        ]
-
-    res.json(objs)
+    Object.find({})
+        .then(objs => res.json(objs))
+        .catch(err => console.error(err + ' error find'))
 })
 
 app.listen(process.env.PORT, () => {
